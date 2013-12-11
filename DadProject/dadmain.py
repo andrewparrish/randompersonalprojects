8f00f34aee43da675e406b72146a257bb771d573
Andrew Parrish <andrew@andrew-linux-desktop.(none)>
2013-12-10 22:51:01 -0500

    small changes

--- DadProject/dadmain.py
+++ DadProject/dadmain.py
@@ -10,10 +10,13 @@ class Realtor:
 			self.email = email
 
 
+#albany realty site
 i = 1
 allhtml = []
-while (i <= 30)
-	html = urllib2.urlopen('http://www.albanyboardofrealtors.com/default.asp?content=agents&menu_id=235349&agt_off_option=last_name&page='+i)
+while (i <= 30):
+	html = urllib2.urlopen('http://www.albanyboardofrealtors.com/default.asp?content=agents&menu_id=235349&agt_off_option=last_name&page='+str(i))
 	soup = BS(html)
 	allhtml.append(soup)
 	i+=1
+
+
