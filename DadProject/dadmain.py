894dd0da72df18df134473d1dd4ba8b57ff65cf8
Andrew <andrewlaptop@ubuntu.(none)>
2013-12-21 22:27:46 -0500

    on the right track

--- DadProject/dadmain.py
+++ DadProject/dadmain.py
@@ -3,10 +3,14 @@ from BeautifulSoup import BeautifulSoup as BS
 import urllib2
 
 class Realtor:
-	def __init__(self, name, phone, address, email):
+	def __init__(self, name, title, phone, street, city, zipcode, state, email):
 			self.name = name
+			self.title = title
 			self.phone = phone
-			self.address = address
+			self.street = street
+			self.city = city
+			self.zipcode = zipcode
+			self.state = state
 			self.email = email
 
 
@@ -14,14 +18,19 @@ class Realtor:
 def albany():
 	i = 1
 	allhtml = []
+	realtors = []
 	while (i <= 2):
 		html = urllib2.urlopen('http://www.albanyboardofrealtors.com/default.asp?content=agents&menu_id=235349&agt_off_option=last_name&page='+str(i))
 		soup = BS(html)
 		matches = soup.findAll('td')
 		for match in matches:
-			print match
-			print "########################################"
-			if 'agent_heading_inline' in match:
+			if 'agent_heading_inline' in str(match):
 				allhtml.append(match)
 		i+=1
-	return allhtml
+	for tag in allhtml:
+		namegrab = tag.findAll('div', {'class' : 'agent_heading_inline'})
+		name = namegrab[0].getText()
+		info = tag.findAll('div', {'class' : 'agent_info_inline'})
+	
+
+	return allhtml	
